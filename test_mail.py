f2f82ea37c503975d9102fda782cdc718e58e7d4
goodhelper01 <goodhelper01@gmail.com>
2025-01-04 10:34:42 +0800

    v4-4

--- test_mail.py
+++ test_mail.py
@@ -2,10 +2,12 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
+from email.mime.base import MIMEBase
+from email import encoders
 
 sender="goodhelper01@gmail.com"
 receiver=["newdragon21@gmail.com","newdragon29@gmail.com"]
-appmima="npgq vzgy mssy wbhh"
+appmima="idci zwub rtqj msev"
 
 for i in receiver:    
     msg=MIMEMultipart()
@@ -18,8 +20,17 @@ for i in receiver:
 
     msg_text=MIMEText(body+body2)
     msg.attach(msg_text)
-    c=ssl.create_default_context()
 
+    text_path = "test.txt"    
+    
+    with open(text_path,"r") as f:
+        part = MIMEBase("application","octet-stream")
+        part.set_payload(f.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition','attachment; filename="test.txt"')
+        msg.attach(part)
+    
+    c=ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com",465,context=c)as server:
         server.login(sender,appmima)
         server.sendmail(sender,i,msg.as_string())
